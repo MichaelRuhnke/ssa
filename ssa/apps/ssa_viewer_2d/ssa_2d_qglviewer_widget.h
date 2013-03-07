10a421df6943380348b3b69b1eb3303c15a6627a
Michael Ruhnke <michael@ruhnke.net>
2013-03-07 17:35:53 +0100

    - added viewer for 2d optimization systems (like 2D SLAM datasets) - changed default number of hierarchies to 5 for pcl point clouds

---
+++ ssa/apps/ssa_viewer_2d/ssa_2d_qglviewer_widget.h
@@ -0,0 +1,127 @@
+#include "ssa/core/ssa_graph_2d.h"
+#include <GL/gl.h>
+#include <GL/glut.h>
+#include <QGLViewer/qglviewer.h>
+
+namespace ssa {
+  using namespace Eigen;
+  using namespace std;
+
+class SSA2DglWidget: public QGLViewer
+{
+  Q_OBJECT
+  public:
+    SSA2DglWidget();
+    SSA2DglWidget(QWidget*& widget);
+    ~SSA2DglWidget();
+
+  inline void setInitialParams(){
+    _backgroundHeight = -0.01;
+    _height = 0.01;
+    _landmarkSize = 0.01;
+    _poseSize = 0.2;
+    _optimize = false;
+    _iterations = 10;
+    _DAmaxdist = 0.2;
+    _pointSize = 0.01;
+    _drawVertices = true;
+    _drawLandmarkVertices = true;
+    _drawNormals = true;
+    _drawCorrespondences = true;
+    _drawMesh = true;
+    _drawGrid = false;
+    _prune = false;
+    _solveDA = false;
+    setShortcut(EXIT_VIEWER, Qt::Key_Q);
+    setSnapshotCounter(0);
+    _level = 0;
+  }
+
+  void init();
+
+  void draw();
+  virtual void drawWithNames();
+  void drawEdges();
+  void drawCovariance();
+  void drawMeshEdges();
+  void drawVertices();
+
+  /** draws circle with radius r at the current pose
+  */
+  void drawEllipsoid(double& l1, double& l2, double& l3);
+  void drawCircle(GLfloat radius, int segments);
+
+  void drawPoseCircle(double& x, double& y, double& theta);
+  void drawPoseCircleWithName(double& x, double& y, double& z);
+
+  void drawNode(Vector2d& pose);
+  void drawGrid();
+  void drawPoseBox();
+  void drawBox(GLfloat l, GLfloat w, GLfloat h);
+
+  void postSelection(const QPoint& point);
+
+  inline void setMap(SparseSurfaceAdjustmentGraph2D* accurateMap){
+    _ssa_graph = accurateMap;
+    Gen3DObjectList_update();
+  }
+
+  GLint Gen3DObjectList_poseVertices();
+  GLint Gen3DObjectList_obsVertices();
+  GLint Gen3DObjectList_normals();
+  GLint Gen3DObjectList_mesh();
+
+  public slots:
+      void setOptimize();
+      inline void prune() {_prune = true; };
+      inline void solveDA() {_solveDA = true; };
+      void openFile();
+      void saveFile();
+      inline void setIterations(int it){ _iterations = it;};
+      inline void setDAMaxDist(double i){ _DAmaxdist = i;};
+      inline void setPointSize(double i){ _pointSize = i; updateGL(); cerr << "pointSize " << _pointSize << endl; Gen3DObjectList_update();};
+      inline void setDrawGrid(int i){ _drawGrid = (bool) i; updateGL();}
+      inline void setDrawVertices(int i){ _drawVertices = (bool) i; updateGL();}
+      inline void setDrawLandmarkVertices(int i){ _drawLandmarkVertices = (bool) i; updateGL();}
+      inline void setDrawNormals(int i){ _drawNormals = (bool) i; updateGL();}
+      inline void setDrawCorrespondences(int i){ _drawCorrespondences = (bool) i; updateGL();}
+      inline void setDrawMesh(int i){ _drawMesh = (bool) i; updateGL();}
+      inline void setStrategie(int i){ std::cerr << __PRETTY_FUNCTION__ << " not implemented!" << i << std::endl;}
+      inline void saveFMap(){ std::cerr << __PRETTY_FUNCTION__ << " not implemented!" << std::endl;}
+      inline void setDrawLevel(int i){ _level = i;};
+      void saveSnapshotCustom();
+      void saveSnapshotVideo();
+      void Gen3DObjectList_update();
+      void Gen3DObjectList_updateSelection();
+
+  public:
+  QWidget*               _parent;
+  SparseSurfaceAdjustmentGraph2D*  _ssa_graph;
+
+  bool _optimize;
+  bool _drawGrid;
+  bool _drawVertices;
+  bool _drawLandmarkVertices;
+  bool _drawNormals;
+  bool _drawCorrespondences;
+  bool _drawMesh;
+  bool _prune;
+  bool _solveDA;
+  GLint _poseVertices;
+  GLint _obsVertices;
+  GLint _normals;
+  GLint _mesh;
+
+  unsigned int _iterations;
+  double _DAmaxdist;
+
+  double _backgroundHeight;
+  double _height;
+  double _landmarkSize;
+  double _poseSize;
+  double _pointSize;
+  qglviewer::Vec _selectedPoint;
+  int     _level;
+};
+
+}
---
+++ ssa/apps/ssa_viewer_2d/ssa_viewer_2d.cpp
@@ -0,0 +1,219 @@
+// Sparse Surface Optimization
+// Copyright (C) 2011 M. Ruhnke, R. Kuemmerle, G. Grisetti, W. Burgard
+// 
+// SSA is free software: you can redistribute it and/or modify
+// it under the terms of the GNU Lesser General Public License as published
+// by the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+// 
+// SSA is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU Lesser General Public License for more details.
+// 
+// You should have received a copy of the GNU Lesser General Public License
+// along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+#include <iostream>
+#include <signal.h>
+#include <QApplication>
+#include <QtGui/QMainWindow>
+#include "ui_base_main_window.h"
+
+#include "ssa/core/allocate_solver.h"
+#include "ssa/core/ssa_graph_2d.h"
+#include "ssa/core/sparse_surface_adjustment.h"
+#include "ssa/data_association/local_refinement.h"
+//#include "ssa/solvers/pcg_cuda/linear_solver_pcg_cuda.h"
+// #include "ssa/solvers/pcg_cusp/linear_solver_pcg_cusp.h"
+
+using namespace std;
+using namespace ssa;
+
+static bool running = true;
+unsigned int breakCounter = 0;
+
+void sighandler(int sig)
+{
+    cout << endl << "Signal " << sig << " caught..." << endl;
+
+   running = false;
+   breakCounter++;
+   if(breakCounter >= 3){
+      exit(1);
+   }
+}
+
+
+int main(int argc, char **argv)
+{
+  signal(SIGINT, &sighandler);
+  QApplication qapp(argc, argv);
+  // HACK reset numeric locale, we need C
+  setlocale (LC_NUMERIC,"C");
+  //bool debug = false;
+  bool restoreViewerState = false;
+  bool dumpScreenshots = false;
+  bool saveOutput = false;
+  char *outfile = 0;
+
+  const char* logfile=0;
+  const char* viewerStateFile=0;
+  bool useConfigFile = false;
+  const char* configFile=0;
+  int c=1;
+  while (c<argc){
+    if (!strcmp(argv[c],"-cam")){
+      restoreViewerState=true;
+      c++;
+      viewerStateFile = argv[c];
+      c++;
+    } else 
+    if (!strcmp(argv[c],"-ini")){
+      useConfigFile=true;
+      c++;
+      configFile = argv[c];
+      c++;
+    } else 
+    if (!strcmp(argv[c],"-video")){
+      dumpScreenshots=true;
+      c++;
+      viewerStateFile = argv[c];
+      c++;
+    } else 
+    if (!strcmp(argv[c],"-save")){
+      saveOutput=true;
+      c++;
+      outfile = argv[c];
+      c++;
+    } else 
+    if (! logfile){
+      logfile=argv[c];
+      c++;
+      break;
+    }
+  }
+
+  //initialize optimizer
+  SparseSurfaceAdjustmentT<g2o::EdgeSE2, ssa::EdgeSE2PointXYCov, ssa::EdgePointXYCovPointXYCov> ssa;
+  ssa.setVerbose(true);
+  g2o::BlockSolverX::LinearSolverType* linearSolver = AllocateLinearSolver<g2o::BlockSolverX>(1); //CHOLMOD
+  ssa.setSolver(linearSolver);
+
+  if(useConfigFile){
+    ifstream configStream(configFile);
+    ssa.params().readParams(configStream);
+    configStream.close();
+    ssa.params().printParams();
+  }
+
+  if (! logfile){
+
+  } else {
+    ssa.graph()->load(logfile);
+  }
+
+  ssa.graph()->fillNeighborCache(ssa.params());
+
+    QMainWindow* mw = new QMainWindow;
+    Ui_MainWindow umw;
+    umw.setupUi(mw);
+
+    umw.ssaGLWidget->setMap(ssa.graph());
+    mw->show();
+    umw.ssaGLWidget->setBackgroundColor(qRgb(211, 211, 211));
+    if(restoreViewerState){
+      umw.ssaGLWidget->setStateFileName(viewerStateFile);
+      umw.ssaGLWidget->restoreStateFromFile();
+    }
+
+    umw.ssaGLWidget->_iterations = ssa.params().ssaIterations;
+    umw.doubleSpinBox_3->setValue(ssa.params().normalShooting.stepSize); 
+    umw.doubleSpinBox_4->setValue(ssa.params().normalShooting.steps); 
+    umw.doubleSpinBox_5->setValue(ssa.params().normalShooting.maxAngleDifference);
+
+    QObject::connect( &(ssa.graph()->_optimizer), SIGNAL(iterationDone()), umw.ssaGLWidget, SLOT(Gen3DObjectList_update()));
+    QObject::connect( &(ssa.graph()->_optimizer), SIGNAL(iterationDone()), umw.ssaGLWidget, SLOT(updateGL()));
+
+    if(dumpScreenshots)
+      QObject::connect( &(ssa.graph()->_optimizer), SIGNAL(iterationDone()), umw.ssaGLWidget, SLOT(saveSnapshotVideo()));
+    bool needReDraw = true;
+
+    if(ssa.graph()->_verticies_poses.size() > 0){
+      g2o::VertexSE2* v = ssa.graph()->_verticies_poses[0];
+
+      qglviewer::Vec initialNodePose = qglviewer::Vec(v->estimate().translation()(0), v->estimate().translation()(1), 20.0);
+      umw.ssaGLWidget->camera()->setPosition(initialNodePose);
+
+      initialNodePose = qglviewer::Vec(v->estimate().translation()(0), v->estimate().translation()(1), 0.0f);
+      umw.ssaGLWidget->camera()->setSceneCenter(initialNodePose);
+
+      umw.ssaGLWidget->camera()->setUpVector(qglviewer::Vec(0.0, 0.0, 1.0));
+      umw.ssaGLWidget->camera()->lookAt(initialNodePose);
+    }
+    cerr << ssa.graph()->_verticies_poses.size() << endl;
+    ///rebuild / build display lists
+    umw.ssaGLWidget->Gen3DObjectList_update();
+
+    while(mw->isVisible() && running) {
+       qapp.processEvents();
+       if(umw.ssaGLWidget->_optimize){
+          umw.ssaGLWidget->_optimize = false;
+
+        if(dumpScreenshots)
+          umw.ssaGLWidget->saveSnapshotVideo();
+
+        umw.stateLabel->setText(QString("running"));
+        QPalette palette;
+        palette.setColor(umw.stateLabel->backgroundRole(), Qt::yellow);
+        umw.stateLabel->setPalette(palette);
+        umw.stateLabel->setAutoFillBackground(true);
+        mw->repaint();
+        qapp.processEvents();
+
+        //filling selected gui params into ssa params
+        ssa.params().ssaIterations = umw.ssaGLWidget->_iterations;
+        //ssa.params().g2oIterations = 6;
+        ssa.params().normalShooting.stepSize = umw.doubleSpinBox_3->value(); 
+        ssa.params().normalShooting.steps = (int) umw.doubleSpinBox_4->value(); 
+        ssa.params().normalShooting.maxAngleDifference = umw.doubleSpinBox_5->value();
+        ssa.params().printParams();
+        cerr << "optimizing level " << umw.spinBox_2->value() << endl;
+        ssa.optimize(umw.spinBox_2->value());
+        //ssa.optimize(ssa.graph()->getMaxLevel());
+        umw.ssaGLWidget->Gen3DObjectList_update();
+        umw.ssaGLWidget->updateGL();
+	  
+        umw.stateLabel->setText(QString("ready"));
+        palette.setColor(umw.stateLabel->backgroundRole(), Qt::green);
+        umw.stateLabel->setPalette(palette);
+
+        umw.stateLabel->setAutoFillBackground(true);
+        needReDraw = true;
+
+        if(dumpScreenshots){
+          umw.ssaGLWidget->Gen3DObjectList_update();
+          umw.ssaGLWidget->updateGL();
+          needReDraw = false;
+          umw.ssaGLWidget->saveSnapshotVideo();
+        }
+
+        if(saveOutput){
+          cerr << "wrote resulting graph to " << outfile << endl;
+          ssa.graph()->save(outfile);
+        }
+
+
+//         cerr << "calculation took " << get_time() - startTime << " seconds." << endl;
+       }
+
+
+
+       if(needReDraw){
+         umw.ssaGLWidget->updateGL();
+         needReDraw = false;
+       }
+       usleep(1000);
+    }
+  return 0;
+}
