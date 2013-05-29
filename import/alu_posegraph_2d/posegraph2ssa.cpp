8988b34363336917456737d5052c80036b46a113
Michael Ruhnke <michael@ruhnke.net>
2013-05-29 18:40:17 +0200

    SSA2D -> SSA

--- import/alu_posegraph_2d/posegraph2ssa.cpp
+++ import/alu_posegraph_2d/posegraph2ssa.cpp
@@ -1,12 +1,12 @@
-// Sparse Surface Optimization 2D
+// Sparse Surface Optimization
 // Copyright (C) 2011 M. Ruhnke, R. Kuemmerle, G. Grisetti, W. Burgard
 //
-// SSA2D is free software: you can redistribute it and/or modify
+// SSA is free software: you can redistribute it and/or modify
 // it under the terms of the GNU Lesser General Public License as published
 // by the Free Software Foundation, either version 3 of the License, or
 // (at your option) any later version.
 //
-// SSA2D is distributed in the hope that it will be useful,
+// SSA is distributed in the hope that it will be useful,
 // but WITHOUT ANY WARRANTY; without even the implied warranty of
 // MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
 // GNU Lesser General Public License for more details.
