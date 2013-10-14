63854c6d002fc4af617b5fe9021e67e50e1971b6
Michael Ruhnke <ruhnke@cs.uni-freiburg.de>
2013-10-14 14:22:33 +0200

    updated information matrix parsing to current g2o order

--- import/alu_posegraph_2d/import_posegraph2d.cpp
+++ import/alu_posegraph_2d/import_posegraph2d.cpp
@@ -58,8 +58,8 @@ namespace ssa {
         SE2 measurement(x,y,theta);
 
         Matrix3d m = Matrix3d::Identity();
-        ls >> m(0, 0) >> m(0, 1) >> m (1, 1)
-           >> m(2, 2) >> m(0, 2) >> m (1, 2);
+        ls >> m(0, 0) >> m(0, 1) >> m (0, 2)
+           >> m(1, 1) >> m(1, 2) >> m (2, 2);
         m(1, 0)=m(0, 1);
         m(2, 0)=m(0, 2);
         m(2, 1)=m(1, 2);
